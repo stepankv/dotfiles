3cee0608465c30ddc89845e1544f9934640a70ca
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2025-05-02 10:14:38 +0300

    Change yazi shortcuts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias ls="eza --color=always --long --icons=always --no-user --no-time --no-permissions"
 alias lzd='lazydocker'
-alias lg='lazygit'
+alias gi='lazygit'
 alias cl='clear'
 alias zj='zellij'
 
