a70091d1615987ee3248f583cec153ccea734691
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2025-09-28 18:00:42 +0300

    (yazi) Add file status plugin

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,1 +1,11 @@
 require("no-status"):setup()
+
+-- https://github.com/yazi-rs/plugins/tree/main/git.yazi#advanced
+th.git = th.git or {}
+th.git.modified = ui.Style():fg("#70AEFF")
+th.git.added = ui.Style():fg("#73BD79")
+th.git.untracked = ui.Style():fg("#E88F89")
+th.git.ignored = ui.Style():fg("#D69A6B")
+th.git.deleted = ui.Style():fg("#6F737A")
+th.git.updated = ui.Style():fg("#DE6A66")
+require("git"):setup()
