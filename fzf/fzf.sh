4a90e39f8eeae5adbbaf04ff01606f31db309a2b
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2025-11-22 16:54:24 +0300

    (theme) Fix separator style

--- fzf/fzf.sh
+++ fzf/fzf.sh
@@ -9,8 +9,8 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
   --color=fg:#DFE1E5,fg+:#d0d0d0,bg:-1,bg+:#2E436E
   --color=hl:#BA9752,hl+:#BA9752,info:#6F737A,marker:#548AF7
   --color=prompt:#CED0D6,spinner:#548AF7,pointer:#5c8ff6,header:#6F737A
-  --color=gutter:#1E2022,border:#393B41,separator:#393B41,scrollbar:#525355
-  --color=preview-bg:#1E2022,preview-border:#393B41,preview-scrollbar:#525355,label:#6F737A
+  --color=gutter:#1E2022,border:#323438,separator:#323438,scrollbar:#525355
+  --color=preview-bg:#1E2022,preview-border:#323438,preview-scrollbar:#525355,label:#6F737A
   --color=query:#BCBEC4"
 
 export FZF_DEFAULT_COMMAND="fd \
